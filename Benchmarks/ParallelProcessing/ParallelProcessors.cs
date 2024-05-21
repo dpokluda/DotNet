cc7009a9bbd0ea0948e86c1900100ee18d8999f4
David Pokluda <davidpokluda@microsoft.com>
2024-05-21 12:36:33 -0700

    Updating parallel processing benchmarks

--- Benchmarks/ParallelProcessing/ParallelProcessors.cs
+++ Benchmarks/ParallelProcessing/ParallelProcessors.cs
@@ -7,22 +7,107 @@ namespace ParallelProcessing;
 [MemoryDiagnoser]
 public class ParallelProcessors
 {
-    private static IEnumerable<MyData> Data = Enumerable.Range(0, 250).Select(i => new MyData()
+    private const int Size = 200;
+    private static IEnumerable<MyData> Data = Enumerable.Range(0, Size).Select(i => new MyData()
     {
         Name = $"name: {i}"
     });
     
     [Benchmark]
-    public void Option1_BlockingCollection()
+    public void Option1_ConcurrentQueue()
     {
-        var output = new List<MyData>(100);
-        foreach (var o in Option1_BlockingCollection_Translate(Data))
+        var output = new List<MyData>(Size);
+        foreach (var o in Option1_ConcurrentQueue_Translate(Data))
         {
             output.Add(o);
         }
     }
     
-    private IEnumerable<MyData> Option1_BlockingCollection_Translate(IEnumerable<MyData> rows)
+    private IEnumerable<MyData> Option1_ConcurrentQueue_Translate(IEnumerable<MyData> rows)
+    {
+        var taskQueue = new ConcurrentQueue<Task<MyData>>();
+        Parallel.ForEach(rows, row =>
+        {
+            Task<MyData> processingTask = ProcessAsync(row);
+            taskQueue.Enqueue(processingTask);
+        });
+
+        while (!taskQueue.IsEmpty)
+        {
+            Task<MyData> task;
+            while (taskQueue.TryDequeue(out task))
+            {
+                if (task.IsCompleted)
+                {
+                    MyData result = task.Result; // Synchronously get the result
+                    if (result != null)
+                    {
+                        yield return result;
+                    }
+                }
+                else
+                {
+                    // If the task is not completed, re-enqueue it for later processing
+                    taskQueue.Enqueue(task);
+                    Task.Delay(10).Wait(); // Introduce a small delay to reduce tight looping
+                }
+            }
+        }
+    }
+    
+    [Benchmark]
+    public void Option2_ConcurrentQueue()
+    {
+        var output = new List<MyData>(Size);
+        foreach (var o in Option2_ConcurrentQueue_Translate(Data))
+        {
+            output.Add(o);
+        }
+    }
+    
+    private IEnumerable<MyData> Option2_ConcurrentQueue_Translate(IEnumerable<MyData> rows)
+    {
+        var taskQueue = new ConcurrentQueue<Task<MyData>>();
+        Parallel.ForEach(rows, row =>
+        {
+            Task<MyData> processingTask = ProcessAsync(row);
+            taskQueue.Enqueue(processingTask);
+        });
+
+        while (!taskQueue.IsEmpty)
+        {
+            Task<MyData> task;
+            while (taskQueue.TryDequeue(out task))
+            {
+                if (task.IsCompleted)
+                {
+                    MyData result = task.Result; // Synchronously get the result
+                    if (result != null)
+                    {
+                        yield return result;
+                    }
+                }
+                else
+                {
+                    // If the task is not completed, re-enqueue it for later processing
+                    taskQueue.Enqueue(task);
+                    Task.Delay(10).Wait(); // Introduce a small delay to reduce tight looping
+                }
+            }
+        }
+    }
+    
+    [Benchmark]
+    public void Option3_BlockingCollectionContinueWith()
+    {
+        var output = new List<MyData>(Size);
+        foreach (var o in Option3_BlockingCollectionContinueWith_Translate(Data))
+        {
+            output.Add(o);
+        }
+    }
+    
+    private IEnumerable<MyData> Option3_BlockingCollectionContinueWith_Translate(IEnumerable<MyData> rows)
     {
         var blockingCollection = new BlockingCollection<MyData>();
         var tasks = new List<Task>();
@@ -51,160 +136,60 @@ public class ParallelProcessors
     }
     
     [Benchmark]
-    public void Option2_ParallelForBlockingCollection()
+    public void Option4_BlockingCollectionAwait()
     {
-        var output = new List<MyData>(100);
-        foreach (var o in Option2_ParallelForBlockingCollection_Translate(Data))
+        var output = new List<MyData>(Size);
+        foreach (var o in Option4_BlockingCollectionAwait_Translate(Data))
         {
             output.Add(o);
         }
     }
     
-    private IEnumerable<MyData> Option2_ParallelForBlockingCollection_Translate(IEnumerable<MyData> rows)
+    private IEnumerable<MyData> Option4_BlockingCollectionAwait_Translate(IEnumerable<MyData> rows)
     {
         var blockingCollection = new BlockingCollection<MyData>();
+        var tasks = new List<Task>();
 
-        Parallel.ForEach(rows, row =>
+        Task enqueueTask = Task.Run(async () =>
         {
-            var result = Process(row);
-            if (result is not null)
+            foreach (var row in rows)
             {
-                blockingCollection.Add(result);
+                var addToCollectionTask = Task.Run(async () =>
+                {
+                    var result = await ProcessAsync(row);
+                    if (result != null)
+                    {
+                        blockingCollection.Add(result);
+                    }
+                });
+                tasks.Add(addToCollectionTask);
             }
+        
+            await Task.WhenAll(tasks);
+            blockingCollection.CompleteAdding();
         });
-
-        // Mark the BlockingCollection as complete for adding.
-        blockingCollection.CompleteAdding();
 
         foreach (var item in blockingCollection.GetConsumingEnumerable())
         {
             yield return item;
         }
-    }
-
-    [Benchmark]
-    public void Option3_Channel()
-    {
-        var output = new List<MyData>(100);
-        foreach (var o in Option3_Channel_Translate(Data))
-        {
-            output.Add(o);
-        }
-    }    
-    private IEnumerable<MyData> Option3_Channel_Translate(IEnumerable<MyData> rows)
-    {
-        var channel = Channel.CreateUnbounded<MyData>();
-        var completionTask = Option3_Channel_ProcessRowsAsync(rows, channel);
-
-        foreach (var item in Option3_Channel_ReadAll(channel.Reader, completionTask))
-        {
-            yield return item;
-        }
-
-        // Ensure all rows are processed before completing the method.
-        completionTask.Wait();
-    }
-
-    private async Task Option3_Channel_ProcessRowsAsync(IEnumerable<MyData> rows, Channel<MyData> channel)
-    {
-        var tasks = new List<Task>();
-
-        foreach (var row in rows)
-        {
-            tasks.Add(Task.Run(async () =>
-            {
-                var result = Process(row);
-                if (result is not null)
-                {
-                    await channel.Writer.WriteAsync(result);
-                }
-            }));
-        }
-
-        await Task.WhenAll(tasks);
-        channel.Writer.Complete();
-    }
-
-    private IEnumerable<MyData> Option3_Channel_ReadAll(ChannelReader<MyData> reader, Task completionTask)
-    {
-        while (true)
-        {
-            MyData item;
-            while (reader.TryRead(out item))
-            {
-                yield return item;
-            }
-
-            if (completionTask.IsCompleted && reader.Count == 0)
-            {
-                yield break;
-            }
-
-            // Introduce a small delay to avoid busy-waiting
-            Task.Delay(10).Wait();
-        }
-    }
-
-    [Benchmark]
-    public void Option4_WhenAny()
-    {
-        var output = new List<MyData>(100);
-        foreach (var o in Option4_WhenAny_Translate(Data))
-        {
-            output.Add(o);
-        }
-    }    
-    
-    private IEnumerable<MyData> Option4_WhenAny_Translate(IEnumerable<MyData> rows)
-    {
-        var tasks = rows.Select(row => Task.Run(() => Process(row))).ToList();
-        var completedTasks = new HashSet<Task<MyData>>();
-
-        while (tasks.Count > 0)
-        {
-            var completedTask = Task.WhenAny(tasks).Result;
-            tasks.Remove(completedTask);
-            completedTasks.Add(completedTask);
-
-            if (completedTask.Result != null)
-            {
-                yield return completedTask.Result;
-            }
-        }
+        
+        enqueueTask.Wait();
     }
     
-    [Benchmark]
-    public void Option5_WhenAnyHashset()
-    {
-        var output = new List<MyData>(100);
-        foreach (var o in Option5_WhenAnyHashset_Translate(Data))
-        {
-            output.Add(o);
-        }
-    }    
-    
-    private IEnumerable<MyData> Option5_WhenAnyHashset_Translate(IEnumerable<MyData> rows)
-    {
-        var tasks = rows.Select(row => Task.Run(() =>
-        {
-            return Process(row);
-        })).ToHashSet();
 
-        while (tasks.Count > 0)
-        {
-            var completedTask = Task.WhenAny(tasks).Result;
-            tasks.Remove(completedTask);
-
-            if (completedTask.Result != null)
-            {
-                yield return completedTask.Result;
-            }
-        }
-    }
- 
     private MyData Process(MyData data)
     {
-        Task.Delay(100).GetAwaiter().GetResult();
+        Task.Delay(Size).GetAwaiter().GetResult();
+        return new MyData
+        {
+            Name = data.Name
+        };
+    }    
+    
+    private async Task<MyData> ProcessAsync(MyData data)
+    {
+        await Task.Delay(Size);
         return new MyData
         {
             Name = data.Name
